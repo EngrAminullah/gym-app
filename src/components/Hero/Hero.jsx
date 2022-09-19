9398365795c8026ddfcb8b2f15a7c7fdadb8726f
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-09-19 19:00:43 +0500

    basic styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="left-h">Left- side</div>
+      <div className="left-h">
+        <Header />
+        {/* the best add */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+      </div>
       <div className="right-h">Right side</div>
     </div>
   );
